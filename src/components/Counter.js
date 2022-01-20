2c68f361bf2da7dd45b57f5e4c36074c9b07f157
iremakdgn <irem.akdgn.1996@gmail.com>
2022-01-20 21:13:18 +0300

    sayaç uygulaması

---
+++ src/components/Counter.js
@@ -0,0 +1,16 @@
+import {useState} from 'react';
+
+function Counter(){
+
+    const [count, setCount]=useState(0);
+
+    return(
+        <div>
+            <h1>{count}</h1>
+            <button onClick={()=> setCount(count+1)}>İncrease</button>
+            <button onClick={()=> setCount(count-1)}>Decrease</button>
+            </div>
+    )
+}
+
+export default Counter;
