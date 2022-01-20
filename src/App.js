23ff3495ee495945657bdd473ae1a4c81ab695d3
iremakdgn <irem.akdgn.1996@gmail.com>
2022-01-20 20:22:02 +0300

    state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { useState } from 'react';
+
 function App() {
+  const [name, setName] =useState("Mehmet"); //state i oluşturma
+  const [age, setAge]= useState(24);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Merhaba {name}</h1>
+      <h2>{age}</h2>
+      <button onClick={()=> setName("Ahmet")}>Change name</button>
+      <button onClick={()=> setAge(30)}>Change age</button>
     </div>
   );
 }
