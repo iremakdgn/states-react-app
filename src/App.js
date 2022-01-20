977d0554384735ce0c223c7bceec7771af18180c
iremakdgn <irem.akdgn.1996@gmail.com>
2022-01-20 21:03:52 +0300

    array and object states

--- src/App.js
+++ src/App.js
@@ -6,12 +6,36 @@ import { useState } from 'react';
 function App() {
   const [name, setName] =useState("Mehmet"); //state i oluşturma
   const [age, setAge]= useState(24);
+  const [friends, setFriens] =useState(['Ahmet','Murat']);
+  const [address, setAddress] = useState({title:"Ataşehir", zip:5555})
+
+
   return (
     <div className="App">
       <h1>Merhaba {name}</h1>
       <h2>{age}</h2>
       <button onClick={()=> setName("Ahmet")}>Change name</button>
       <button onClick={()=> setAge(30)}>Change age</button>
+
+      <hr></hr>
+
+      <br></br>
+      <h2>Friends</h2>
+
+      {
+        friends.map((friend,key)=> <div key={key}>{friend}</div>)
+      }
+
+<button onClick={()=> setFriens([...friends,'Ayşe','İrem'])}>Change age</button>
+
+
+<hr></hr>
+
+<br></br>
+<h2>Adres</h2>
+<div>{address.title} {address.zip}</div>
+
+<button onClick={()=> setAddress({...address, title:'Ankara'})}>Change adress</button>
     </div>
   );
 }
